c8bf82717f9fb4a9d967f14dbb9f056944a3b8e9
Saanvi-22 <83111088+Saanvi-22@users.noreply.github.com>
2022-06-28 12:37:44 +0530

    Add files via upload

---
+++ project-c25-template-main/Paper.js
@@ -0,0 +1,17 @@
+class paper
+{
+constructor(x,y){
+
+    this.image =loadImage("paper.png");
+    this.imageWidth=10;
+    this.imageHeight = 10;
+
+    var options={
+        isStatic:false,
+        restitution:0.3,
+        friction:0,
+        density:1.2
+      
+    }
+}
+}
